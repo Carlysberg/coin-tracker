4aba062434ceeb284f398c12f2b55aa48b341ad2
Kuwuamenu Kofi Gabriel <74659884+Carlysberg@users.noreply.github.com>
2023-07-21 19:33:00 -0700

    added the drop down menu

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
+  const PriceScreen({super.key});
+
   @override
-  _PriceScreenState createState() => _PriceScreenState();
+  PriceScreenState createState() => PriceScreenState();
 }
 
-class _PriceScreenState extends State<PriceScreen> {
+class PriceScreenState extends State<PriceScreen> {
+
+  String pickedCurrency = 'GHC';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Rate'),
+        title: const Text('🤑 Coin Rate'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
               color: Colors.lightBlueAccent,
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
-              child: Padding(
+              child: const Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
                   '1 BTC = ? USD',
@@ -40,9 +44,20 @@ class _PriceScreenState extends State<PriceScreen> {
           Container(
             height: 150.0,
             alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
+            padding: const EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton(
+                value: pickedCurrency,
+                items: const [
+                  DropdownMenuItem(value: 'USD' ,child: Text('USD')),
+                  DropdownMenuItem(value: 'GHC' ,child: Text('GHC')),
+                  DropdownMenuItem(value: 'GBP' ,child: Text('GBP')),
+                ],
+                onChanged: (newValue){
+                  setState(() {
+                    pickedCurrency = newValue!;
+                  });
+    }),
           ),
         ],
       ),
